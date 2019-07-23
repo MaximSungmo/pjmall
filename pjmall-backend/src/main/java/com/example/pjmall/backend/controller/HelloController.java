2e6f2914edae2d85c1ae316b38f3377dc3c1717c
MaximSungmo <ksm5318@naver.com>
2019-07-23 10:50:13 +0900

    OAuth2 적용

--- pjmall-backend/src/main/java/com/example/pjmall/backend/controller/HelloController.java
+++ pjmall-backend/src/main/java/com/example/pjmall/backend/controller/HelloController.java
@@ -3,8 +3,11 @@ package com.example.pjmall.backend.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.pjmall.backend.domain.User;
 import com.example.pjmall.backend.dto.JSONResult;
 
 @RestController
@@ -12,9 +15,26 @@ public class HelloController {
 	
 	@GetMapping("/hello")
 	public ResponseEntity<JSONResult> hello(){
+		
 		return ResponseEntity
 				.status(HttpStatus.OK)
 				.body(JSONResult.success("Hello World"));
+	}
+	
+	@GetMapping("/hello2")
+	public ResponseEntity<JSONResult> hello2(){
+		
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(JSONResult.success("Hello World2"));
+	}
+	
+	@PostMapping("/hello3")
+	public ResponseEntity<JSONResult> hello3(@RequestBody User user){
+		
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(JSONResult.success("Hello World3"));
 	}
 	
 }
--- pjmall-backend/src/test/java/com/example/pjmall/backend/controller/test/ControllerTest.java
+++ pjmall-backend/src/test/java/com/example/pjmall/backend/controller/test/ControllerTest.java
@@ -1,14 +1,32 @@
 package com.example.pjmall.backend.controller.test;
 
-import static org.junit.Assert.assertNotNull;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.oauth2.common.util.JacksonJsonParser;
+import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.example.pjmall.backend.domain.User;
+import com.google.gson.Gson;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,9 +36,86 @@ public class ControllerTest {
 	@Autowired
     MockMvc mockMvc;
 	
-	@Test
-	public void test() {
-		assertNotNull(mockMvc);
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+	
+	@Autowired
+	private FilterChainProxy springSecurityFilterChain;
+	
+	private String accessToken; 
+	
+	@Before	
+	public void setUp() throws Exception {
+		mockMvc = MockMvcBuilders.
+			webAppContextSetup(webApplicationContext)
+			.addFilters(springSecurityFilterChain)
+			.build();
 		
+		if(accessToken != null) {
+			return;
+		}
+		
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>(); 
+		params.add("grant_type", "password");
+		params.add("client_id", "pjmall");
+		params.add("username", "test");
+		params.add("password", "1234");
+		params.add("scope", "MALL_USER");
+		
+		ResultActions resultActions =
+		mockMvc
+			.perform(post("/oauth/token")
+			.params(params)
+			.with(httpBasic("pjmall", "1234"))
+			.contentType(MediaType.APPLICATION_JSON))
+			.andDo(print())
+			.andExpect(status().isOk());
+		
+		String resultString = resultActions.andReturn().getResponse().getContentAsString();
+		JacksonJsonParser jsonParser = new JacksonJsonParser();
+		accessToken = jsonParser.parseMap(resultString).get("access_token").toString();
+	}
+	
+	@Ignore
+	@Test
+	public void testHelloUnauthorized() throws Exception {
+		mockMvc
+			.perform(get("/hello"))
+			.andDo(print())
+			.andExpect(status().isUnauthorized());
+	}
+	
+	@Test
+	public void testHelloAuthorized() throws Exception {
+		mockMvc
+			.perform(
+				get("/hello")
+				.header("Authorization", "bearer " + accessToken))
+			.andDo(print())
+			.andExpect(status().isOk());
+	}
+	
+//	@Test
+//	public void testHello2() throws Exception {
+//		mockMvc
+//			.perform(
+//				get("/hello2"))
+//
+//			.andDo(print())
+//			.andExpect(status().isOk());
+//	}
+	
+	@Test
+	public void testHello3Authorized() throws Exception {
+		User user = new User(1L,"ksm","1234");
+		
+		mockMvc
+			.perform(
+				post("/hello3")
+				.header("Authorization", "bearer " + accessToken)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(new Gson().toJson(user)))
+			.andDo(print())
+			.andExpect(status().isOk());
 	}
 }
